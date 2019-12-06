483f10214b4f87e715e8e917b91bcf5c24534fb7
11001101 <interngithub@gmail.com>
2019-12-05 22:35:12 -0700

    added strings.rb

---
+++ Chapters/TheBasics/strings.rb
@@ -0,0 +1,24 @@
+# Ex. 1: with double quotes
+=begin
+puts "The man said, 'Hi there!'"
+=end
+
+# Ex. 1: with double quotes
+=begin
+puts "The man said, 'Hi there!'"
+=end
+
+# "puts" puts it on the next line and print keeps on going on the same line
+
+# irb 
+=begin
+irb :001 > a = 'ten'
+=> "ten"
+------------
+irb :002 > "My favorite number is #{a}!"
+=> "My favorite number is ten!"
+=end
+
+:name = "Bobby Van Cuvor"
+puts "Hello??"
+puts name
