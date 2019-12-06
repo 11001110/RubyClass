accb5e166d181e296310188f2c88a902842613f2
11001101 <interngithub@gmail.com>
2019-12-05 22:47:47 -0700

    numbers.rb

---
+++ Chapters/TheBasics/numbers.rb
@@ -0,0 +1,15 @@
+# Example of integers
+#1, 2, 3, 50, 10, 4345098098
+
+# Example of floats
+#1.2345, 2345.4267, 98.2234
+
+=begin
+floats and integers
+4, 95, 12.394, 5, 29.384
+=end
+floats = 34.343
+integers = 942
+
+puts floats
+puts integers
